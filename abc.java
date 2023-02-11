80212a3cebc4cb789e4acd9ea42a38e4e0a81658
Pavanyadav1234 <124158711+Pavanyadav1234@users.noreply.github.com>
2023-02-11 17:34:27 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -1,8 +1,17 @@
 class Abc
 {
- int a=5;
- int b=6;
- int c=3;
- int d=a*b*c;
+ void area()
+ {
+  int a=5;
+  int b=6;
+  int c=3;
+  int d=a*b*c;
+  System.out.println(d);
  }
+ public static void main(String[] args);
+ {
+  sample s1=new sample;
+  si.area();
+ }
+}
  
